509074450467e8ea5e6abf9d7b87fffc6d5e25f5
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2023-10-01 16:30:35 +0500

    commit after JDK hometask 1

---
+++ java_development_kit/hometask_1/Main.java
@@ -0,0 +1,10 @@
+package java_development_kit.hometask_1;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        new ConnectWindow(new LogHandler("./Hometask_1_Log"));
+    }
+}
