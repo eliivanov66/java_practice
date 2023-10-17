ffb19cc3855dfb1af4d27ae894f238c8e1fd0722
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2023-10-18 02:58:20 +0500

    commit after hometask 3 v3

--- java_development_kit/hometask_3_1/hometask_3_1/src/Main.java
+++ java_development_kit/hometask_3_1/hometask_3_1/src/Main.java
@@ -12,42 +12,72 @@ public class Main {
         scheme.put("name", "TEXT");
         scheme.put("phone", "INT");
 
-        db.deleteTable("test");
-        db.createTable("test", scheme );
+        db.deleteTable("users");
+        db.createTable("users", scheme );
 
         Map<String,Object> tempMap = new HashMap<>();
         tempMap.put("name", "name#1");
         tempMap.put("phone", 123);
-        db.writeTable("test", tempMap);
+        db.writeTable("users", tempMap);
 
         tempMap = new HashMap<>();
         tempMap.put("name", "name#2");
         tempMap.put("phone", 1234);
-        db.writeTable("test", tempMap);
+        db.writeTable("users", tempMap);
 
         tempMap = new HashMap<>();
         tempMap.put("name", "name#3");
         tempMap.put("phone", 12345);
-        db.writeTable("test", tempMap);
+        db.writeTable("users", tempMap);
 
 
-        List<Map<String, Object>> resultFull = db.readTable("test");
+        List<Map<String, Object>> resultFull = db.readTable("users");
         for (Map<String,Object> map : resultFull) {
             db.printTableRow(map);
         }
 
-        Map<String, Object> resultID = db.readTableByID("test", 3);
+        Map<String, Object> resultID = db.readTableByID("users", 3);
         db.printTableRow(resultID);
 
         tempMap = new HashMap<>();
         tempMap.put("name", "name#3updated");
         tempMap.put("phone", 123456);
 
-        db.updateTable("test", tempMap, 3);
+        db.updateTable("users", tempMap, 3);
 
-        resultID = db.readTableByID("test", 3);
+        resultID = db.readTableByID("users", 3);
         db.printTableRow(resultID);
+
+
+        scheme = new HashMap<>();
+        scheme.put("name", "TEXT");
+        scheme.put("quantity", "INT");
+        scheme.put("price", "REAL");
+
+        db.deleteTable("stock");
+        db.createTable("stock", scheme);
+
+        tempMap = new HashMap<>();
+        tempMap.put("name", "product#1");
+        tempMap.put("quantity", 10);
+        tempMap.put("price", 1.4);
+        db.writeTable("stock", tempMap);
+
+        tempMap = new HashMap<>();
+        tempMap.put("name", "product#2");
+        tempMap.put("quantity", 30);
+        tempMap.put("price", 11.5);
+        db.writeTable("stock", tempMap);
+
+        tempMap = new HashMap<>();
+        tempMap.put("name", "product#3");
+        tempMap.put("quantity", 4);
+        tempMap.put("price", 200.4);
+        db.writeTable("stock", tempMap);
+
+
         db.disconnect();
+
     }
 
 }
