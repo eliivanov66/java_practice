f47564e5de274af04ce873c44ed0b902c758db23
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2023-10-27 11:43:16 +0500

    commit after hometask 3 v3

---
+++ java_development_kit/seminar_4/task_1/Element.java
@@ -0,0 +1,17 @@
+public class Element{
+    private Object element;
+    private Element nextElement;
+    private Element prevElement;
+
+    public Element getNext(){
+        return this.nextElement;
+    }
+
+    public Element getPreveus(){
+        return this.prevElement;
+    }
+
+    public void set(Object o){
+        element = o;
+    }
+}
---
+++ java_development_kit/seminar_4/task_1/MyList.java
@@ -0,0 +1,25 @@
+public class MyList {
+    private Element firstElement;
+    private Element lastElement;
+    public MyList(){
+
+    }
+
+    public void add(Element element){
+        if (this.firstElement == null) {
+            firstElement = element;
+            lastElement = element;
+            
+            firstElement.getNext().set(null);
+            firstElement.getPreveus().set(null);
+
+            lastElement.getNext().set(null);
+            lastElement.getPreveus().set(null);
+        } else {
+            lastElement.getNext().set(element);
+            element.getNext().set(null);
+            element.getPreveus().set(lastElement);
+            lastElement = element;
+        }
+    } 
+}
