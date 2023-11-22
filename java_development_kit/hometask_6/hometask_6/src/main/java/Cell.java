212e46beadbc05ba333bfb9822a316101358ff44
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2023-11-22 21:01:10 +0500

    commit after hometask6

---
+++ java_development_kit/hometask_6/hometask_6/src/main/java/Cell.java
@@ -0,0 +1,25 @@
+public class Cell {
+    private boolean value = false;
+
+
+
+    private boolean hidden = true;
+    Cell(){
+        value = false;
+    }
+    public void setValue(boolean arg) {
+        value = arg;
+    }
+
+    public boolean getValue(){
+        return value;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public boolean getHidden(){
+        return hidden;
+    }
+}
